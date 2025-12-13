773b8df2a257365e30d06d149b3e0bf2d12fcfa6
Udit <uditkapoor060@gmail.com>
2025-12-13 16:16:49 +0530

    updated the deployed backend url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "http://localhost:5454"
+export const API_BASE_URL = "https://e-commerce-backend-production-e39d.up.railway.app"
 const jwt = localStorage.getItem("jwt");
 
 export const api = axios.create({
